23e1a1cc0bed597d93102253a72940ad8b51c4ee
jhon pardo <jhonedupardo15@gmail.com>
2026-01-08 11:24:49 -0500

    Problema 2

--- ejercicios/Problemas/src/paquete02/Problema2.java
+++ ejercicios/Problemas/src/paquete02/Problema2.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package paquete02;
+import java.util.Scanner;
 
 /**
  *
@@ -16,6 +17,49 @@ public class Problema2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String[] viviendas = obtenerViviendas();
+        double[][] consumos = obtenerConsumosMensuales();
+        double[] totales = ConsumoAnual(consumos);
+        mostrarReporte(viviendas, totales);
+    
     }
     
-}
+    public static String[] obtenerViviendas() {
+        Scanner entrada = new Scanner(System.in);
+        String[] viviendas = new String[10];
+
+        for (int i = 0; i < viviendas.length; i++) {
+            System.out.print("Ingrese el nombre de la vivienda1");
+            viviendas[i] = entrada.nextLine();
+        }
+
+        return viviendas;
+    }
+    public static double[][] obtenerConsumosMensuales() {
+         Scanner entrada = new Scanner(System.in);
+        double[][] consumos = new double[10][12];
+
+        for (int i = 0; i < consumos.length; i++) {
+            System.out.println("Consumos de la vivienda " + (i + 1));
+            for (int j = 0; j < consumos[i].length; j++) {
+                System.out.print("Mes " + (j + 1) + ": ");
+                consumos[i][j] = entrada.nextDouble();
+            }
+        }
+
+        return consumos;
+    }
+
+    public static double[] ConsumoAnual(double[][] consumos) {
+
+        double[] totales = new double[consumos.length];
+
+        for (int i = 0; i < consumos.length; i++) {
+            double suma = 0;
+            for (int j = 0; j < consumos[i].length; j++) {
+                suma = suma + consumos[i][j];
+            }
+            totales[i] = suma;
+        }
+
+     
